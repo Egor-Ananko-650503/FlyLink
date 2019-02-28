781b50eba29b73b236540e8352c29b0a690c6b6a
Egor Ananko <egorananko@gmail.com>
2019-02-28 17:29:31 +0300

    Initial project commit

---
+++ src/main/java/by/bsuir/flylink/FlyLinkApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.flylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsuir/flylink/FlyLinkApplicationTests.java
@@ -0,0 +1,16 @@
+package by.bsuir.flylink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyLinkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
