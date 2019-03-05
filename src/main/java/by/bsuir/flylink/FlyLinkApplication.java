8d76d2d0f3fe625e5860eeccfbecbb11da22e3aa
Svetlana Reznichenko <srezrez@gmail.com>
2019-03-05 18:21:52 +0300

    @EnableConfigurationProperties annotation was added

--- src/main/java/by/bsuir/flylink/FlyLinkApplication.java
+++ src/main/java/by/bsuir/flylink/FlyLinkApplication.java
@@ -1,13 +1,17 @@
 package by.bsuir.flylink;
 
+import by.bsuir.flylink.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class FlyLinkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlyLinkApplication.class, args);
     }
-
 }
